e389493f5c7256ec360922443a28a31be26b341e
WhitePhosphorus4 <1038478003@qq.com>
2020-05-07 17:50:40 +0800

    修改了makefile中的一个bug

--- source/inc.h
+++ source/inc.h
@@ -2,4 +2,4 @@ extern int tag[3];
 void calculate(int t, int num, string s, vector<int> &arr);
 void getOrder(size_t t, vector<int> &arr, int &cnt)   ;
 bool judge(string s);
-bool cha(int num)
+bool cha(int num);
